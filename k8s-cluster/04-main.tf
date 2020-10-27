fd7c84152a1073fda71895b75b25970e2d929bd1
systemctl0 <habib.paz@globant.com>
2020-10-27 13:15:04 -0300

    Updated tf files

--- k8s-cluster/04-main.tf
+++ k8s-cluster/04-main.tf
@@ -1,7 +1,8 @@
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
-  project  = var.project_id
+  name            = var.cluster_name
+  project         = var.project_id
+  location        = var.region
+  node_locations  = [ var.zone ]
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
@@ -10,3 +11,27 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
  
 }
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = var.node_pool_name
+  location   = var.region
+  project    = var.project_id
+  cluster    = google_container_cluster.primary.name
+
+  node_count = var.node_count
+
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
